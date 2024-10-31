a2b440c99bd1beac572d8210b764392a9f99a31d
Brianleach11 <81440466+Brianleach11@users.noreply.github.com>
2024-10-31 12:27:49 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,6 @@ naive_bayes_model = load_model('nb_model.pkl')
 random_forest_model = load_model('rf_model.pkl')
 decision_tree_model = load_model('dt_model.pkl')
 knn_model = load_model('knn_model.pkl')
-voting_classifier_model = load_model('voting_model.pkl')
 
 def prepare_input_data(credit_score, location, gender, age, tenure, balance, num_products, has_credit_card, is_active_member, estimated_salary):
     input_dict = {
@@ -53,7 +52,6 @@ def make_prediction(input_df, input_dict):
         'Random Forest': random_forest_model.predict_proba(input_df)[0, 1],
         'Decision Tree': decision_tree_model.predict_proba(input_df)[0, 1],
         'K-Nearest Neighbors': knn_model.predict_proba(input_df)[0, 1],
-        'Voting Classifier': voting_classifier_model.predict_proba(input_df)[0, 1]
     }
     avg_probability = np.mean(list(probabilities.values()))
 
